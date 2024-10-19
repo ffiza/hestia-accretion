2a49567278ad5a64cf603e6d34d022c930bc008c
Federico Iza <fedegiza@gmail.com>
2024-10-19 10:27:29 -0300

    Add colors and line styles for galaxies.

--- hestia/settings.py
+++ hestia/settings.py
@@ -5,3 +5,5 @@ class Settings:
 
     def __init__(self) -> None:
         self.galaxies: list = ["17_11_MW", "17_11_M31"]
+        self.galaxy_colors: list = ["tab:blue", "tab:red"]
+        self.galaxy_lss: list = ["-", "--"]
