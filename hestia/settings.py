450959ea542af5ec27a3cfa760b19136c7df921c
Federico Iza <fedegiza@gmail.com>
2024-10-19 15:35:24 -0300

    Update galaxy colors and line styles.

--- hestia/settings.py
+++ hestia/settings.py
@@ -4,6 +4,15 @@ class Settings:
     """
 
     def __init__(self) -> None:
-        self.galaxies: list = ["17_11_MW", "17_11_M31"]
-        self.galaxy_colors: list = ["tab:blue", "tab:red"]
-        self.galaxy_lss: list = ["-", "--"]
+        self.galaxies: list = [
+            "17_11_MW", "17_11_M31",
+            "09_18_MW", "09_18_M31",
+            "37_11_MW", "37_11_M31"]
+        self.galaxy_colors: dict = {
+            "17_11_MW": "tab:red", "17_11_M31": "tab:red",
+            "09_18_MW": "tab:green", "09_18_M31": "tab:green",
+            "37_11_MW": "tab:blue", "37_11_M31": "tab:blue"}
+        self.galaxy_lss: dict = {
+            "17_11_MW": "-", "17_11_M31": "--",
+            "09_18_MW": "-", "09_18_M31": "--",
+            "37_11_MW": "-", "37_11_M31": "--"}
