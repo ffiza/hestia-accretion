152692ec6e11abd4940b9e71284c639a45fddcc8
Federico Iza <fedegiza@gmail.com>
2025-07-10 13:42:13 -0300

    Make all members static.

--- hestia/settings.py
+++ hestia/settings.py
@@ -1,8 +1,4 @@
 class Settings:
-    """
-    A class to manage generic configuration variables.
-    """
-
     SIMULATIONS: list = ["17_11", "09_18", "37_11"]
     GALAXIES: list = ["MW", "M31"]
     SIMULATION_COLORS: dict = {
@@ -14,17 +10,3 @@ class Settings:
         "MW": "-",
         "M31": "--"
     }
-
-    def __init__(self) -> None:
-        self.galaxies: list = [
-            "17_11_MW", "17_11_M31",
-            "09_18_MW", "09_18_M31",
-            "37_11_MW", "37_11_M31"]
-        self.galaxy_colors: dict = {
-            "17_11_MW": "tab:red", "17_11_M31": "tab:red",
-            "09_18_MW": "tab:green", "09_18_M31": "tab:green",
-            "37_11_MW": "tab:blue", "37_11_M31": "tab:blue"}
-        self.galaxy_lss: dict = {
-            "17_11_MW": "-", "17_11_M31": "--",
-            "09_18_MW": "-", "09_18_M31": "--",
-            "37_11_MW": "-", "37_11_M31": "--"}
