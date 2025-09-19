f593fea20068ab2c499027aee7019d7275026335
Federico Iza <fedegiza@gmail.com>
2025-09-19 15:11:16 -0300

    Add new figure.

--- visualisation/prop_comparison.py
+++ visualisation/prop_comparison.py
@@ -1,8 +1,10 @@
+import json
 import yaml
 import argparse
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.stats import linregress
 
 from hestia.images import figure_setup
 from hestia.settings import Settings
@@ -57,6 +59,60 @@ def _get_auriga_data(config: dict) -> pd.DataFrame:
         )
     df["Delta1200"] = present_day_delta
 
+    return df
+
+
+def _get_data_for_corr_analysis(snapnum: int, config: dict) -> pd.DataFrame:
+    galaxies = []
+
+    sfr = []
+    stellar_mass = []
+    delta = []
+
+    for galaxy in range(1, 31):
+        data = pd.read_csv("data/iza_et_al_2022/sfr.csv")
+        sfr.append(data[f"SFR_Au{galaxy}_Msun/yr"].to_numpy()[snapnum - 1])
+        data = pd.read_csv(f"data/auriga/au{galaxy}/baryon_mass.csv", sep=" ")
+        stellar_mass.append(data["TotalStar"].to_numpy()[snapnum])
+        data = pd.read_csv("data/iza_et_al_2022/environment_delta_1200.csv")
+        delta.append(data[f"Delta1200_Au{galaxy}"].to_numpy()[snapnum])
+        galaxies.append(f"Au{galaxy}")
+    for simulation in Settings.SIMULATIONS:
+        for galaxy in Settings.GALAXIES:
+            data = pd.read_csv(
+                f"data/hestia/{galaxy}_M_SFR_t_Hestia{simulation}.csv")
+            sfr.append(data["SFR"][data["SnapNo"] == snapnum].values[0])
+            stellar_mass.append(
+                data["Mstar"][data["SnapNo"] == snapnum].values[0] / 1e10)
+            environment = pd.read_csv(
+                f"results/{simulation}_{galaxy}/delta_1200.csv")
+            delta.append(
+                environment["Delta"].to_numpy()[snapnum])
+            galaxies.append(f"{simulation}_{galaxy}")
+
+    colors = ["darkgray"] * 30
+    for s in Settings.SIMULATIONS:
+        for _ in Settings.GALAXIES:
+            colors.append(Settings.SIMULATION_COLORS[s])
+
+    symbols = ["o"] * 30
+    for _ in Settings.SIMULATIONS:
+        for g in Settings.GALAXIES:
+            symbols.append(Settings.GALAXY_SYMBOLS[g])
+
+    df = pd.DataFrame({
+        "Galaxy": galaxies,
+        "SFR_Msun/yr": sfr,
+        "Mstar_10^10Msun": stellar_mass,
+        "Delta1200": delta,
+        "Colors": colors,
+        "Symbols": symbols,
+    })
+
+    with open('data/auriga/simulation_data.json', 'r') as file:
+        data = json.load(file)
+    df.time = data["Original"]["Time_Gyr"][snapnum]
+    df.redshift = data["Original"]["Redshift"][snapnum]
     return df
 
 
@@ -162,6 +218,71 @@ def plot_prop_comparison(config: dict) -> None:
     plt.close(fig)
 
 
+def plot_time_correlation(config: dict) -> None:
+    fig, ax = plt.subplots(figsize=(2.5, 2.5))
+
+    ax.set_xlabel("Time [Gyr]")
+    ax.set_ylabel("Regression Slope")
+    ax.set_xlim(0, 14)
+    ax.set_ylim(0, 4)
+    ax.set_xticks([2, 4, 6, 8, 10, 12])
+    ax.set_yticks([0, 1, 2, 3, 4])
+
+    ax1 = ax.inset_axes([1, 0, 1, 1/3])
+    ax1.tick_params(axis="y", labelleft=False, labelright=True)
+    ax1.set_xlabel(r"$\log_{10} \delta_{1200}$")
+    ax1.set_xlim(0, 1.5)
+    ax1.set_ylim(-0.4, 1.6)
+    ax1.set_xticks([0.25, 0.5, 0.75, 1, 1.25])
+    ax1.set_yticks([0, 0.5, 1])
+    ax2 = ax.inset_axes([1, 1/3, 1, 1/3], sharex=ax1, sharey=ax1)
+    ax2.tick_params(axis="x", labelbottom=False)
+    ax2.tick_params(axis="y", labelleft=False, labelright=True)
+    ax2.set_ylabel(
+        r"$\log_{10} \mathrm{SFR} \, [\mathrm{M}_\odot \, \mathrm{yr}^{-1}]$")
+    ax2.yaxis.set_label_position("right")
+    ax3 = ax.inset_axes([1, 2/3, 1, 1/3], sharex=ax1, sharey=ax1)
+    ax3.tick_params(axis="x", labelbottom=False)
+    ax3.tick_params(axis="y", labelleft=False, labelright=True)
+
+    axs = [ax1, ax2, ax3]
+    snapnums = [77, 95, 127]
+
+    for i, snapnum in enumerate(snapnums):
+        df = _get_data_for_corr_analysis(snapnum, config)
+        for _, row in df.iterrows():
+            axs[i].scatter(
+                np.log10(row["Delta1200"]), np.log10(row["SFR_Msun/yr"]), s=14,
+                color=row["Colors"], facecolors="none", marker=row["Symbols"])
+        r = linregress(np.log10(df["Delta1200"]), np.log10(df["SFR_Msun/yr"]))
+        axs[i].text(0.025, 0.95, f"$z =$ {round(df.redshift, 1)}",
+                    transform=axs[i].transAxes,
+                    ha="left", va='top', fontsize=6)
+        axs[i].plot(axs[i].get_xlim(),
+                    [r.slope * axs[i].get_xlim()[0] + r.intercept,
+                     r.slope * axs[i].get_xlim()[1] + r.intercept],
+                    c="black", lw=1)
+
+    with open('data/auriga/simulation_data.json', 'r') as file:
+        data = json.load(file)
+    time = data["Original"]["Time_Gyr"]
+
+    slopes = [np.nan] * 128
+    pvalues = [np.nan] * 128
+    for snapnum in range(50, 128):
+        df = _get_data_for_corr_analysis(snapnum, config)
+        r = linregress(np.log10(df["Delta1200"]), np.log10(df["SFR_Msun/yr"]))
+        slopes[snapnum] = r.slope
+        pvalues[snapnum] = r.pvalue
+    ax.scatter(time, slopes, c=pvalues, s=10)
+    for snapnum in snapnums:
+        ax.scatter(time[snapnum], slopes[snapnum], color="red", s=10,
+                   facecolor="none")
+
+    plt.savefig("images/prop_correlation.png")
+    plt.close(fig)
+
+
 if __name__ == "__main__":
     figure_setup()
 
@@ -173,4 +294,5 @@ if __name__ == "__main__":
     # Load configuration file
     config = yaml.safe_load(open(f"configs/{args.config}.yml"))
 
-    plot_prop_comparison(config)
+    # plot_prop_comparison(config)
+    plot_time_correlation(config)
